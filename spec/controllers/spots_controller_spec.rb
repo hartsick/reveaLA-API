0090aa381c321a867e97af68615f4d055b97a882
Christa Hartsock <christa.hartsock@gmail.com>
2014-06-21 18:10:19 -0700

    begin spot controller spec

--- spec/controllers/spots_controller_spec.rb
+++ spec/controllers/spots_controller_spec.rb
@@ -16,24 +16,42 @@ RSpec.describe SpotsController, :type => :controller do
     end
   end
 
-  describe "PUT 'update'" do
-    it "returns http success" do
-      put 'update'
-      expect(response).to be_success
+  describe "POST 'create'" do
+    describe 'logged in user or admin' do
+      it "returns http success" do
+      end
+    end
+    describe 'not logged in' do
+      it "should not be authorized" do
+        post :create, id: spot.id
+        response.status.should == 401
+      end
     end
   end
 
-  describe "POST 'create'" do
-    it "returns http success" do
-      post 'create'
-      expect(response).to be_success
+  describe "PUT 'update'" do
+    describe 'logged in admin' do
+      it "returns http success" do
+      end
+    end
+    describe 'not admin' do
+      it "should not be authorized" do
+        put :update, id: spot.id
+        response.status.should == 401
+      end
     end
   end
 
   describe "POST 'destroy'" do
-    it "returns http success" do
-      post 'destroy'
-      expect(response).to be_success
+    describe 'logged in admin' do
+      it "returns http success" do
+      end
+    end    
+    describe 'not admin' do
+      it "should not be authorized" do
+        post :destroy, id: spot.id
+        response.status.should == 401
+      end
     end
   end
 
