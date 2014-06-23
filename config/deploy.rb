2a8648ed38534e62e112411c23c441e7b6029519
Christa Hartsock <christa.hartsock@gmail.com>
2014-06-23 13:10:08 -0700

    Secrets add to deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,7 +9,7 @@ set :scm_verbose, true
 set :application, 'reveaLAAPI'
 set :repo_url, 'git@github.com:hartsick/reveaLA-API.git'
 set :branch, 'master'
-set :linked_files, ['config/database.yml']
+set :linked_files, ['config/database.yml', 'config/secrets.yml']
 set :linked_dirs, ['log', 'db_backups']
 set :deploy_via, :remote_cache
 set :rvm_ruby_version, '2.1.1'
