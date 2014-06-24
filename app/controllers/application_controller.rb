b8e9f3a4f966796067c93431f3687d52f6d12089
Christa Hartsock <christa.hartsock@gmail.com>
2014-06-24 16:08:40 -0700

    allow all origins

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,7 +12,7 @@ class ApplicationController < ActionController::Base
 
   # For all responses in this controller, return the CORS access control headers.
   def cors_set_access_control_headers
-    headers['Access-Control-Allow-Origin'] = 'http://kelseyledford.github.io/reveaLA-angular/*'
+    headers['Access-Control-Allow-Origin'] = '*'
     headers['Access-Control-Allow-Methods'] = 'PATCH, DELETE, POST, GET, OPTIONS'
     headers['Access-Control-Max-Age'] = "1728000"
   end
@@ -22,7 +22,7 @@ class ApplicationController < ActionController::Base
   # text/plain.
 
   def cors_preflight_check
-    headers['Access-Control-Allow-Origin'] = 'http://kelseyledford.github.io/reveaLA-angular/*'
+    headers['Access-Control-Allow-Origin'] = '*'
     headers['Access-Control-Allow-Methods'] = 'PATCH, DELETE, POST, GET, OPTIONS'
     headers['Access-Control-Allow-Headers'] = 'X-Requested-With, X-Prototype-Version, Content-Type'
     headers['Access-Control-Max-Age'] = '1728000'
